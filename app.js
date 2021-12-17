a1c2611d63fc54a2816d6d0f43e1d6ffa3746cb4
Alicia Kmak <alicia.kmak@gmail.com>
2021-12-17 16:02:59 +0100

    add iteration 2. Stopped working after session set up was added

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ require('dotenv/config');
 
 // ℹ️ Connects to the database
 require('./db');
-
+const MONGO_URI = require('./db/index')
 // Handles http requests (express is node js framework)
 // https://www.npmjs.com/package/express
 const express = require('express');
@@ -14,6 +14,7 @@ const express = require('express');
 const hbs = require('hbs');
 
 const app = express();
+const session =require('express-session')
 
 // ℹ️ This function is getting exported from the config folder. It runs most middlewares
 require('./config')(app);
@@ -29,7 +30,26 @@ const index = require('./routes/index');
 app.use('/', index);
 
 const authRouter = require("./routes/auth"); 
+const MongoStore = require('connect-mongo');
+const cookieParser =require('cookie-parser');
 app.use("/", authRouter);
+
+//session setup
+app.use(cookieParser());
+app.use(
+    session({
+        store: MongoStore.create({
+            mongoUrl: MONGO_URI,
+            ttl: 24 * 60 * 60
+        }),
+        secret: process.env.SECRET,
+        resave: true,
+        saveUninitialized: false,
+        cookie: {
+            maxAge: 24 * 60 * 60
+        }
+    })
+)
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app);
